33676510ca664ad989570adcaf77d8c11627d5ae
Mateusz Jaruga <ul0253575@edu.uni.lodz.pl>
2022-06-01 10:57:19 +0200

    Added redirectingUser class

--- Administration/RedirectingUser.cs
+++ Administration/RedirectingUser.cs
@@ -6,34 +6,70 @@ using System.Threading.Tasks;
 
 namespace Administration
 {
-    class RedirectingUser
+    public class RedirectingUser
     {
-        public static string RedirectUserIfLogged()
+        public static string RedirectUser(string activeURL, bool isChecked)
         {
-            return null;
+            if (!LogSys.CheckIfLogged())
+                return "~/Pages/MainPages/Default";
+
+
+            if (URLLibrary(activeURL))
+                return activeURL;
+
+            string site = RedirectToCorrespondingSite();
+            return RedirectToCorrespondingSite();
         }
 
-        public static string RedirecUserIfNotLogged()
+        private static string RedirectToCorrespondingSite()
         {
-            //Checking whether the user is already logged in
-            if (LogSys.CheckIfLogged())
+            switch (MySession.Current.Position)
             {
-                switch (MySession.Current.Position)
-                {
-                    case "administrator":
-                        return "~/Pages/AdministratorPages/AdministratorPanelPage";
-                    case "doctor":
-                        return "~/Pages/Doctorspages/DoctorPanelPage";
-                    case "medical clinic staff member":
-                        return "~/Pages/medicalStaffMemberPages/MedicalStaffPanelPage";
-                    case "patient management staff":
-                        return "~/Pages/PatientManagementStaffPages/PatientManagementPanelPage";
-                    case "manager":
-                        return "~/Pages/ManagerPages/ManagerPanelPage";
-                }
+                case "administrator":
+                    return "~/Pages/AdministratorPages/AdministratorPanelPage";
+                case "doctor":
+                    return "~/Pages/Doctorspages/DoctorPanelPage";
+                case "medical clinic staff member":
+                    return "~/Pages/medicalStaffMemberPages/MedicalStaffPanelPage";
+                case "patient management staff":
+                    return "~/Pages/PatientManagementStaffPages/PatientManagementPanelPage";
+                case "manager":
+                    return "~/Pages/ManagerPages/ManagerPanelPage";
+                default:
+                    return "";
+            }
+        }
+
+        private static bool URLLibrary(string activeURL)
+        {
+            string[] path = activeURL.Split('/');
+            switch (MySession.Current.Position)
+            {
+                case "administrator":
+                    if (path[2].Contains("AdministratorPages"))
+                        return true;
+                    return false;
+                case "doctor":
+                    if (path[2].Contains("DoctorsPages"))
+                        return true;
+                    return false;
+                case "medical clinic staff member":
+                    if (path[2].Contains("MedicalStaffMemberPages"))
+                        return true;
+                    return false;
+                case "patient management staff":
+                    if (path[2].Contains("PatientManagementStaffPages"))
+                        return true;
+                    return false;
+                case "manager":
+                    if (path[2].Contains("ManagerPages"))
+                        return true;
+                    return false;
+
+                default:
+                    return false;
             }
 
-            return null;
         }
     }
 }
